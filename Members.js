ec0867951f96e7bd6af6093ebd82d2e7a5578619
Atul Beniwal <atulbeniwal15@gmail.com>
2020-05-23 11:57:16 +0530

    express-js

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "John Cena",
+    email: "john@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Bob Williams",
+    email: "bob@gmail.com",
+    status: "inactive",
+  },
+  {
+    id: 3,
+    name: "Micheal Jackson",
+    email: "micheal@gmail.com",
+    status: "active",
+  },
+];
+
+module.exports = members;
